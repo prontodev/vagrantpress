95d8b045a91659393c0155a87d7481f4eb98b4d6
prontokan <kan@prontomarketing.com>
2014-03-22 20:04:39 +0700

    remove unused commands

--- setup/bootstrap_local.sh
+++ setup/bootstrap_local.sh
@@ -5,24 +5,3 @@ source /etc/profile.d/rvm.sh
 rvm requirements
 rvm install 2.0.0
 gem install --no-rdoc --no-ri cucumber capybara rspec selenium-webdriver headless
-
-su vagrant <<'EOF'
-echo 'export WORKON_HOME=$HOME/.virtualenvs' >> /home/vagrant/.bash_profile
-echo 'export PIP_VIRTUALENV_BASE=$WORKON_HOME' >> /home/vagrant/.bash_profile
-echo 'source /usr/local/bin/virtualenvwrapper.sh' >> /home/vagrant/.bash_profile
-source /usr/local/bin/virtualenvwrapper.sh
-source /home/vagrant/.bash_profile
-mkvirtualenv pronto-vagrantpress-env
-workon pronto-vagrantpress-env
-pip install -r /vagrant/requirements/local.txt
-EOF
-echo "Pronto VagrantPress environment is set up"
-
-su vagrant <<'EOF'
-mkdir /home/vagrant/log/
-EOF
-cp /vagrant/setup/local/pronto-dashboard.nginx /etc/nginx/sites-enabled/.
-rm /etc/nginx/sites-enabled/default
-cp /vagrant/setup/local/upstart-pronto-dashboard.conf /etc/init/.
-service nginx restart
-service upstart-pronto-dashboard start
